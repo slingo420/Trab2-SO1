07aa01823a2d761eecfb1d7fa4f6c710173b9d3b
slingo420 <luizgabrielslongo@gmail.com>
2023-10-13 13:45:24 -0300

    num_quadros = num_frames

--- main.cpp
+++ main.cpp
@@ -11,18 +11,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int num_quadros = std::stoi(argv[1]);
+    int num_frames = std::stoi(argv[1]);
 
     Reader reader;
     std::vector<int> ref_vector = reader.read();
 
-    std::cout << "Number of frames: " << num_quadros << std::endl;
+    std::cout << "Number of frames: " << num_frames << std::endl;
     std::cout << "References: " << std::endl;
     for (const int& referencia : ref_vector) {
         std::cout << referencia << std::endl;
     }
 
-    FIFO fifo(num_quadros);
+    FIFO fifo(num_frames);
     int page_faults = fifo.run(ref_vector);
     std::cout << "Page faults: " << page_faults << std::endl;
 
