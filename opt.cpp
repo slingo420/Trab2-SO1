7d2a94f6b8363ccd436f6f7b33b012f850ecf148
Enzo Brum <darosabrumenzo@gmail.com>
2023-10-19 13:45:48 -0300

    arruma opt

--- opt.cpp
+++ opt.cpp
@@ -5,7 +5,7 @@
 #include <map>
 #include <queue>
 #include <functional>
-
+#include <iostream>
 unsigned int OPT::run(const std::vector<int>& ref_vector) {
     unsigned int page_faults = 0;
 
@@ -14,21 +14,30 @@ unsigned int OPT::run(const std::vector<int>& ref_vector) {
     for (unsigned long long i = 0; i < ref_vector.size(); ++i)
         page_references[ref_vector[i]].push(i);
 
+    for (auto&[k, _] : page_references) {
+        page_references[k].push(2e9 - k);
+        page_references[k].pop();
+    }
 
     std::unordered_map<int, int> frame_map;
     std::map<int, int, std::greater<int>> frame_next_ref; // queremos que a página que será lida no maior índice fique em primeirp
     for (int page : ref_vector) {
         auto frame = frame_map.find(page);
         if (frame == frame_map.end()) {
+            page_faults++;
             if (frame_map.size() == num_frames) {
                 auto victim = frame_next_ref.begin();
+
                 frame_map.erase( victim->second );
                 frame_next_ref.erase(victim);
             }
-            page_faults++;
+        }
+        else {
+          frame_next_ref.erase(frame->second);
         }
 
         int next_ref = page_references[page].front();
+        page_references[page].pop();
         frame_map[page] = next_ref;
         frame_next_ref[next_ref] = page;
     }
